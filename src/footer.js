bf4bed589fe02f827e3d29ad6bcee14277e93118
alex <alejandro_n@outlook.com>
2021-12-12 14:26:37 -0600

    fixed waze

--- src/footer.js
+++ src/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function footer() {
   return (
     <div className="footer">
-      <a href="https://www.waze.com/en/live-map/directions/terraza-bambinos-av.-nicolas-copernico-3631-zapopan?place=w.168165582.1681524751.5545780&utm_campaign=iframe%20module&utm_medium=homepage&utm_source=website">
+      <a href="https://waze.com/ul/h9ewmqek45">
         <h3>Abrir dirección en Waze</h3>
         <img
           className="babyWaze"
--- src/header.js
+++ src/header.js
@@ -6,7 +6,7 @@ function header() {
     <div className="headerDiv">
      <h2>Acompañanos a celebrar <br></br>el baby shower de</h2>
      <h1>Sofía</h1>
-     <p>15 de enero 2022 3:00pm <br></br>Terraza Bambinos<br></br>Av. Nicolás Copérnico 3631, Zapopan, Jal </p>
+     <p>15 de enero 2022 3:00pm <br></br>Terraza Bambinos<br></br>Av. Nicolás Copérnico 3631<br></br>Zapopan, Jal </p>
     </div>
   );
 }
