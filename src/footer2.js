10beed8f4031aca860cc03c51748c0a9ee5906bd
alex <alejandro_n@outlook.com>
2022-01-05 00:17:06 -0600

    liverpool number added

--- src/footer2.js
+++ src/footer2.js
@@ -8,6 +8,8 @@ function footer2() {
             
             <a href="https://mesaderegalos.liverpool.com.mx/milistaderegalos/50676310">
                 <img src={liverpool} alt="" />
+                <h2>Mesa # 50676310</h2>
+
             </a>
             <a href="https://www.amazon.com.mx/baby-reg/perla-flores-febrero-2021-guadalajara/24MPZN2J0DH82">
                 <img src={amazon} alt="" />
